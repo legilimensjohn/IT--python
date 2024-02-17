c5e663182db5b7dcfff06c4dce158d0496572b33
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-02-17 21:33:35 +0800

    Update Laboratory_2.py

--- Laboratory_2.py
+++ Laboratory_2.py
@@ -1,38 +1,41 @@
-print('Select operator')
-print('1. Add')
-print('2. Subtract')
-print('3. Multiply')
-print('4. Divide')
+# Basic Calculator
 
-print('Enter choice: ')
+print("Calculator")
+print("Select operator")
+print("[1] Sum")
+print("[2] Difference")
+print("[3] Product")
+print("[4] Quotient")
+
+print("Please provide your selection: ")
 num = input()
 
-if str(num) == '1':
-    print('Num1: ')
+if str(num) == "1":
+    print("Num1: ")
     num1 = input()
-    print('Num2: ')
+    print("Num2: ")
     num2 = input()
-    print('The sum is: ' + str(int(num1)+int(num2)))
-elif str(num) == '2':
-    print('Num1: ')
+    print("The sum is: " + str(int(num1) + int(num2)))
+elif str(num) == "2":
+    print("Num1: ")
     num1 = input()
-    print('Num2: ')
+    print("Num2: ")
     num2 = input()
-    print('The Difference is: ' + str(int(num1)-int(num2)))
-elif str(num) == '3':
-    print('Num1: ')
+    print("The Difference is: " + str(int(num1) - int(num2)))
+elif str(num) == "3":
+    print("Num1: ")
     num1 = input()
-    print('Num2: ')
+    print("Num2: ")
     num2 = input()
-    print('The Product is: ' + str(int(num1)*int(num2)))
-elif str(num) == '4':
-    print('Num1: ')
+    print("The Product is: " + str(int(num1) * int(num2)))
+elif str(num) == "4":
+    print("Num1: ")
     num1 = input()
-    print('Num2: ')
+    print("Num2: ")
     num2 = input()
-    if int (num2) == 0:
-        print ('Invalid Input')
+    if int(num2) == 0:
+        print("Invalid Input")
     else:
-        print('The Quotient is: ' + str(int(num1) / int(num2)))
+        print("The Quotient is: " + str(int(num1) / int(num2)))
 else:
-    print('Invalid input')
+    print("Invalid input")
