025c4f700b361bbbae7e96c71bdc5a132ffef938
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-02-22 02:34:06 +0800

    Update truthTABLE.py

--- truthTABLE.py
+++ truthTABLE.py
@@ -2,3 +2,8 @@ def geberate_truth_table (expression, val_p, val_q):
   header = ['p', 'q', expression]
   print("        | ".join(header))
 
+for p, q in zip(val_p, val_q):
+  if 'not q' in expression:
+    result = not q
+  else:
+    result = evaluation(expression, {'p': p, 'q': q})
