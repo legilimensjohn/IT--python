d0006317698e542c32fa5a0cf64db04e2c5a91fa
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-04-01 22:56:18 +0800

    Update encrypt.py

---
+++ encrypt.py
@@ -0,0 +1,13 @@
+import pyfiglet as pyfiglet
+from os import system as system
+#from textblob import TextBlob
+
+main = py.figletFormat ("       Cryption")
+print (main, "                 [*]encryp & decryp text data's[*]")
+
+def format():
+    main = py.figletFormat ("       Cryption")
+print (main, "                 [*]encryp & decryp text data's[*]")
+
+def encrypt():
+    data
