53e824c9c88dccf62f8eb6f5e2e9299b68861c04
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-04-02 19:58:14 +0800

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -18,4 +18,22 @@ def encrypt():
     x1 = data.encode()
 
     replace1 = len(data)-1//len(encryptHash) + 1
-    y2
+    y2 =(encryptHash * replace1) [:len(data)].encode()
+
+    data = bytes ([i1 i2 for (i1, i2) in zip (x1, y2)])
+    print (f"The Encrypt text is: {data.decode()}")
+
+    def encrypt():
+        data = input('Kindly enter text: ')
+        decrypHash = input('Kindly enter hash: ')
+
+        z3 = data.encode()
+
+        replace2 = len(data)-1//len(decrypHash) + 1
+        w4 = (decrypHash*replace2)[:len(data)].encode()
+
+        data_1 = bytes ([i1 i2 for (i1, i2) in zip (z3, w4)])
+        decryp_data = data_1.decode()
+        data_string = string(decryp_data)
+        data = data_string.replaces("-", "")
+        print(f"The Decrypt text is: {data}")
