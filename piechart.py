3b336d144e88fe2dc8b0885e891335ce18d015c6
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-07-28 21:01:50 +0800

    Update piechart.py

--- piechart.py
+++ piechart.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as pit
 
 #Data to plot
 
-labels 'Labell', 'Label2', 'Label3', 'Label4"]
+labels = 'Labell', 'Label2', 'Label3', 'Label4"]
 
-sizes [15, 30, 45, 10] #These are the sizes or proportions of eachistice
+sizes = [15, 30, 45, 10] #These are the sizes or proportions of eachistice
 
-colors['gold", "yellowgreen', 'Lightcoral, Lightskyblue] explode (0.1, 0, 0, 0) Explode the Ist slice (Le. Label
+colors['gold", "yellowgreen', 'Lightcoral, Lightskyblue] #explode (0.1, 0, 0, 0) Explode the Ist slice (Le. Label
 
 #Create a ple chart
 plt.ple(sizes, explode explode, labels labels, colors colors, autopct=%1.1fW, shadow True, startangle-148)
