cc0ac755064cdff74ff17a7c97b68f5913caa76e
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-07-28 21:00:54 +0800

    Update piechart.py

--- piechart.py
+++ piechart.py
@@ -8,20 +8,16 @@ sizes [15, 30, 45, 10] #These are the sizes or proportions of eachistice
 
 colors['gold", "yellowgreen', 'Lightcoral, Lightskyblue] explode (0.1, 0, 0, 0) Explode the Ist slice (Le. Label
 
-Create a ple chart
-
+#Create a ple chart
 plt.ple(sizes, explode explode, labels labels, colors colors, autopct=%1.1fW, shadow True, startangle-148)
 
 OUTPUT
 
-Equal aspect ratio ensures that pie ts drawn as a circle
-
+#Equal aspect ratio ensures that pie ts drawn as a circle
 plt.axis("equal)
 
-Add a title
-
+#Add a title
 plt.title( 'My Pie Chart')
 
-Display the chart
-
+#Display the chart
 plt.show()
