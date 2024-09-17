fbbbbadafaa63a4fda5c6357e1e8c4d399052e82
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-09-17 21:02:08 +0800

    Update screen_recoder.py

--- screen_recoder.py
+++ screen_recoder.py
@@ -1,1 +1,16 @@
+import pyautogui
+import cv2
+import numpy as np
 
+resolution = (1920, 1080)
+
+code= cv2.VideoWriter_fourcc(*"XVID")
+
+fileName = "Recording.avi"
+
+fps = 60.0
+
+out = cv2.VideoWriter(fileName, code, fps, resolution)
+
+cv2.nameWindow("Live", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Live", 500, 280)
