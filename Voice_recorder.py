fa0784dd9e2870f5eff65f03b19873395030382d
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-09-17 20:54:23 +0800

    Update Voice_recorder.py

--- Voice_recorder.py
+++ Voice_recorder.py
@@ -1,1 +1,17 @@
+import sounddevice as sd
+from scipy.io.wavfile import write
+import wavio as mv
 
+
+#sampling frequency
+freq = 44100
+
+#recording duration - you can modify
+duration = 5
+
+recording = sd.rec(int(duration * freq),
+                   samplerate = freq, channels = 2)
+
+sd.wait()
+
+wv.write("recording1.wa", recording, freq, sampwidth = 2)
