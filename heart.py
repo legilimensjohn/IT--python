600d3670f999fdb1374765805b06ab78fb941fac
John Christian Ventura <130374378+legilimensjohn@users.noreply.github.com>
2024-09-22 23:23:25 +0800

    Update heart.py

--- heart.py
+++ heart.py
@@ -1,10 +1,6 @@
 import math
 
-2
 
-3
-
-3
 
 from turtle import*
 
@@ -20,9 +16,6 @@ math.cos(2*k)-2*\
 
 math.cos(3*k)-\
 
-10
-
-11
 
 math.cos(4*k)
 
@@ -30,26 +23,19 @@ speed (0)
 
 12bgcolor("black")
 
-13
-
-14
 
 for i in range(6000):
 
 goto(hearta(i)*20, heartb(i)*20)
 
-15
 
 for j in range(5):
 
-16
 
 color("red")
 
-17
 
 goto (0,0)
 
-18
 
 done()
